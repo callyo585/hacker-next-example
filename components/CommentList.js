89603c46dbf1a54c89a9b2a08576629caf4cc722
calvin13 <calvin@lavax.co>
2019-11-15 15:04:58 +0800

    heroku-deployment

---
+++ components/CommentList.js
@@ -0,0 +1,8 @@
+import Comment from "./Comment";
+export default ({ comments }) => (
+  <React.Fragment>
+    {comments.map(comment => (
+      <Comment key={comment.id} comment={comment} />
+    ))}
+  </React.Fragment>
+);
---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+import StoryList from "../components/StoryList";
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+class Index extends React.Component {
+  static async getInitialProps({ req, res, query }) {
+    let stories;
+    let page;
+
+    try {
+      page = Number(query.page) || 1;
+      const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
+      stories = await response.json();
+    } catch (error) {
+      console.log(error);
+      stories = [];
+    }
+    return { page, stories };
+  }
+
+  componentDidMount() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(registration => {
+          console.log(registration);
+        })
+        .catch(err => {
+          console.warn(err.message);
+        });
+    }
+  }
+
+  render() {
+    const { page, stories } = this.props;
+
+    if (stories.length == 0) {
+      return <Error statusCode={503} />;
+    }
+    return (
+      <Layout title="Hacker Next" description="A Hacker News clone made with Nextjs">
+        <StoryList stories={stories} />
+
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
+      </Layout>
+    );
+  }
+}
+
+export default Index;
